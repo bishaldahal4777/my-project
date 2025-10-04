6a0ca520cf0e79cc841463fa0de137aaec424a47
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-04 12:37:07 +0545

    second Push

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'post_list.html', {'posts':posts})
+
+def post_detail(request):
+    pass
+
+def create_post(request):
+    pass
