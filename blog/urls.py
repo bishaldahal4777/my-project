748e21c417ade4caf97bec685ed296245aca2ae1
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-04 17:27:01 +0545

    ui/ux changed

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,12 @@ from . import views
 
 urlpatterns = [
     path("posts/", views.post_list, name= "post_list"),
-    path("posts/<int:post_id>/", views.post_detail, name= "post_detail"),
+    path("post/<int:post_id>/", views.post_detail, name= "post_detail"),
     path("create/", views.create_post, name= "create_post"),
+    path("post/<int:post_id>/update/", views.update_post, name= "update_post"),
+    path("post/<int:post_id>/delete/", views.delete_post, name= "delete_post"),
+
+
 
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,16 +8,35 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, post_id):
-    post= get_object_or_404(post, id=post_id)
-    return render(request, 'blog/post_detail.html', {'post':post})
+    post= get_object_or_404(Post, id=post_id)
+    return render(request, 'blog/post_detail.html', {'posts':post})
 
 def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
-
         if form.is_valid():
             form.save()
-        return redirect('post_list')
+            return redirect('post_list')
     else:
         form = PostForm()
-        return redirect(request,'blog/create_post.html', {'form': form})
+    return render(request,'blog/create_post.html', {'form': form})
+    
+
+def update_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect ('blog/post_detail.html', post_id=post.id)
+    else:
+        form = PostForm(instance=Post)
+    return render(request, 'blog/update_post.html', {'form': form})
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        post.delete()
+    return render(request, 'blog/post_list.html', {'post':post})
