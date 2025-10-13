8df5500a4b169cd7258d3ae9327d8e6018ab97fc
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-13 11:34:11 +0545

    new commit try

--- blog/forms.py
+++ blog/forms.py
@@ -6,10 +6,3 @@ class PostForm(forms.ModelForm):
         model=Post
         fields = ['title', 'content','image']
 
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model= Comment
-        fields =['content']
-        widgets = {
-            'content': forms.Textarea(attrs={'rows':3, 'class':'form-control','placeholder':'Write a comment...'})
-        }
